17444739dc19e9985e749f65ca6b7859974240b4
Xu213 <1548689263@qq.com>
2019-11-15 16:14:20 +0800

    新建内容管理

--- admin/src/main.js
+++ admin/src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './plugins/element.js'
+import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 
+import http from "./http/http"
+Vue.prototype.$http = http
+
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  el: '#app',
+  render: h => h(App)
+});
